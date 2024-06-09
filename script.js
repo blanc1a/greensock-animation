fd61a31776edc2256469fdb0ea068e98613fa441
Blanca Ziems <blanca481@hotmail.de>
2024-06-09 13:59:38 +0200

    refactor: content

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
-gsap.to("img", { x: 400, duration: 3 });
+let tl = gsap.timeline();
+tl.from("#demo", { opacity: 0 })
+  .from("h1", { x: 80, opacity: 0 })
+  .from("h2", { x: -80, opacity: 0 })
+  .from("p", { y: 30, opacity: 0 })
+  .from("button", { y: 50, opacity: 0 })
+  .from("#items > g", {
+    transformOrigin: "50% 50%",
+    scale: 0,
+    opacity: 0,
+    stagger: 0.1,
+  });
